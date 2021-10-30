8c4ab9d5ca3328628a3612eaf1541a103926fe5b
Matthias Bruns <git@matthiasbruns.com>
2021-10-30 18:39:07 +0200

    #1 Add FetchDataFeedFromUrl

--- test/client_test.go
+++ test/client_test.go
@@ -309,3 +309,67 @@ func TestFetchDataFeed(t *testing.T) {
 		}
 	}
 }
+
+func TestFetchDataFeedFromUrl(t *testing.T) {
+	// Read mock data from CSV
+	csvContent, err := readCSVFileContents("testdata/data_feed.csv")
+	if err != nil {
+		t.Fatalf("coult not parse csv file '%v'", err)
+	}
+
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	if _, err := gz.Write([]byte(csvContent)); err != nil {
+		t.Error(err)
+	}
+	if err := gz.Flush(); err != nil {
+		t.Error(err)
+	}
+	if err := gz.Close(); err != nil {
+		t.Error(err)
+	}
+
+	// Create mock response
+	response := &http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(b.Bytes())),
+	}
+
+	// Create test client to run tests on
+	awinClient := awin.NewAwinClient(&http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
+		fmt.Println(r.URL.String())
+		expectedUrl := "https://productdata.awin.com/datafeed/download/apikey/apiKey/language/en/fid/fid1,fid2/columns/aw_deep_link,product_name,aw_product_id,merchant_product_id,merchant_image_url,description,merchant_category,search_price,merchant_name,merchant_id,category_name,category_id,aw_image_url,currency,store_price,delivery_cost,merchant_deep_link,language,last_updated,display_price,data_feed_id,brand_name,brand_id,colour,product_short_description,specifications,condition,product_model,model_number,dimensions,keywords,promotional_text,product_type,commission_group,merchant_product_category_path,merchant_product_second_category,merchant_product_third_category,rrp_price,saving,savings_percent,base_price,base_price_amount,base_price_text,product_price_old,delivery_restrictions,delivery_weight,warranty,terms_of_contract,delivery_time,in_stock,stock_quantity,valid_from,valid_to,is_for_sale,web_offer,pre_order,stock_status,size_stock_status,size_stock_amount,merchant_thumb_url,large_image,alternate_image,aw_thumb_url,alternate_image_two,alternate_image_three,alternate_image_four,reviews,average_rating,rating,number_available,custom_1,custom_2,custom_3,custom_4,custom_5,custom_6,custom_7,custom_8,custom_9,ean,isbn,upc,mpn,parent_product_id,product_GTIN,basket_link/format/csv/delimiter/,/compression/gzip/adultcontent/1/"
+		if r.URL.String() != expectedUrl {
+			err := errors.New(fmt.Sprintf("invalid url found in test\nexpected '%s'\nfound '%s'", expectedUrl, r.URL.String()))
+			t.Error(err)
+			return err
+		}
+
+		expectedMethod := "GET"
+		if r.Method != expectedMethod {
+			err := errors.New(fmt.Sprintf("invalid request method in test\nexpected '%s'\nfound '%s'", expectedMethod, r.Method))
+			t.Error(err)
+			return err
+		}
+
+		return nil
+	}}})
+
+	result, err := awinClient.FetchDataFeedFromUrl("https://productdata.awin.com/datafeed/download/apikey/apiKey/language/en/fid/fid1,fid2/columns/aw_deep_link,product_name,aw_product_id,merchant_product_id,merchant_image_url,description,merchant_category,search_price,merchant_name,merchant_id,category_name,category_id,aw_image_url,currency,store_price,delivery_cost,merchant_deep_link,language,last_updated,display_price,data_feed_id,brand_name,brand_id,colour,product_short_description,specifications,condition,product_model,model_number,dimensions,keywords,promotional_text,product_type,commission_group,merchant_product_category_path,merchant_product_second_category,merchant_product_third_category,rrp_price,saving,savings_percent,base_price,base_price_amount,base_price_text,product_price_old,delivery_restrictions,delivery_weight,warranty,terms_of_contract,delivery_time,in_stock,stock_quantity,valid_from,valid_to,is_for_sale,web_offer,pre_order,stock_status,size_stock_status,size_stock_amount,merchant_thumb_url,large_image,alternate_image,aw_thumb_url,alternate_image_two,alternate_image_three,alternate_image_four,reviews,average_rating,rating,number_available,custom_1,custom_2,custom_3,custom_4,custom_5,custom_6,custom_7,custom_8,custom_9,ean,isbn,upc,mpn,parent_product_id,product_GTIN,basket_link/format/csv/delimiter/,/compression/gzip/adultcontent/1/")
+	if err != nil {
+		t.Fatalf("err is not null '%v'", err)
+	}
+
+	if len(*result) != 10 {
+		t.Fatalf("Invalid amount of data rows received %d", len(*result))
+	}
+
+	// Check if received rows and expected rows match
+	expectedRows, _ := parseCSVToDataFeedEntry(csvContent)
+	for i, expectedRow := range *expectedRows {
+		receivedRow := (*result)[i]
+		if expectedRow != receivedRow {
+			t.Fatalf("Invalid row parsed\nexpected '%v'\nreceived '%v'", expectedRow, receivedRow)
+		}
+	}
+}
