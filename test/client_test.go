0b8b6c1f629c174858c8ecc8583b5ca8a6edd89b
Matthias Bruns <git@matthiasbruns.com>
2023-02-17 09:29:21 +0100

    Move api key into client config

--- test/client_test.go
+++ test/client_test.go
@@ -205,7 +205,7 @@ func TestFetchDataFeedList(t *testing.T) {
 	}
 
 	// Create test client to run tests on
-	awinClient := awin.NewAwinClient(&http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
+	awinClient := awin.NewAwinClient("apiKey", &http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
 		expectedUrl := "https://productdata.awin.com/datafeed/list/apikey/apiKey"
 		if r.URL.String() != expectedUrl {
 			err := errors.New(fmt.Sprintf("invalid url found in test\nexpected '%s'\nfound '%s'", expectedUrl, r.URL.String()))
@@ -223,7 +223,7 @@ func TestFetchDataFeedList(t *testing.T) {
 		return nil
 	}}})
 
-	result, err := awinClient.FetchDataFeedList("apiKey")
+	result, err := awinClient.FetchDataFeedList()
 	if err != nil {
 		t.Fatalf("err is not null '%v'", err)
 	}
@@ -268,7 +268,7 @@ func TestFetchDataFeed(t *testing.T) {
 	}
 
 	// Create test client to run tests on
-	awinClient := awin.NewAwinClient(&http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
+	awinClient := awin.NewAwinClient("apiKey", &http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
 		expectedUrl := "https://productdata.awin.com/datafeed/download/apikey/apiKey/language/en/fid/fid1,fid2/columns/aw_deep_link,product_name,aw_product_id,merchant_product_id,merchant_image_url,description,merchant_category,search_price,merchant_name,merchant_id,category_name,category_id,aw_image_url,currency,store_price,delivery_cost,merchant_deep_link,language,last_updated,display_price,data_feed_id,brand_name,brand_id,colour,product_short_description,specifications,condition,product_model,model_number,dimensions,keywords,promotional_text,product_type,commission_group,merchant_product_category_path,merchant_product_second_category,merchant_product_third_category,rrp_price,saving,savings_percent,base_price,base_price_amount,base_price_text,product_price_old,delivery_restrictions,delivery_weight,warranty,terms_of_contract,delivery_time,in_stock,stock_quantity,valid_from,valid_to,is_for_sale,web_offer,pre_order,stock_status,size_stock_status,size_stock_amount,merchant_thumb_url,large_image,alternate_image,aw_thumb_url,alternate_image_two,alternate_image_three,alternate_image_four,reviews,average_rating,rating,number_available,custom_1,custom_2,custom_3,custom_4,custom_5,custom_6,custom_7,custom_8,custom_9,ean,isbn,upc,mpn,parent_product_id,product_GTIN,basket_link/format/csv/delimiter/,/compression/gzip/adultcontent/1/"
 		if r.URL.String() != expectedUrl {
 			err := errors.New(fmt.Sprintf("invalid url found in test\nexpected '%s'\nfound '%s'", expectedUrl, r.URL.String()))
@@ -287,7 +287,6 @@ func TestFetchDataFeed(t *testing.T) {
 	}}})
 
 	result, err := awinClient.FetchDataFeed(&awin.DataFeedOptions{
-		ApiKey:           "apiKey",
 		FeedIds:          []string{"fid1", "fid2"},
 		Language:         "en",
 		ShowAdultContent: true,
@@ -339,7 +338,7 @@ func TestFetchDataFeedFromUrl(t *testing.T) {
 	}
 
 	// Create test client to run tests on
-	awinClient := awin.NewAwinClient(&http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
+	awinClient := awin.NewAwinClient("apiKey", &http.Client{Transport: mockRoundTripper{response: response, requestTestFunc: func(r *http.Request) error {
 		fmt.Println(r.URL.String())
 		expectedUrl := "https://productdata.awin.com/datafeed/download/apikey/apiKey/language/en/fid/fid1,fid2/columns/aw_deep_link,product_name,aw_product_id,merchant_product_id,merchant_image_url,description,merchant_category,search_price,merchant_name,merchant_id,category_name,category_id,aw_image_url,currency,store_price,delivery_cost,merchant_deep_link,language,last_updated,display_price,data_feed_id,brand_name,brand_id,colour,product_short_description,specifications,condition,product_model,model_number,dimensions,keywords,promotional_text,product_type,commission_group,merchant_product_category_path,merchant_product_second_category,merchant_product_third_category,rrp_price,saving,savings_percent,base_price,base_price_amount,base_price_text,product_price_old,delivery_restrictions,delivery_weight,warranty,terms_of_contract,delivery_time,in_stock,stock_quantity,valid_from,valid_to,is_for_sale,web_offer,pre_order,stock_status,size_stock_status,size_stock_amount,merchant_thumb_url,large_image,alternate_image,aw_thumb_url,alternate_image_two,alternate_image_three,alternate_image_four,reviews,average_rating,rating,number_available,custom_1,custom_2,custom_3,custom_4,custom_5,custom_6,custom_7,custom_8,custom_9,ean,isbn,upc,mpn,parent_product_id,product_GTIN,basket_link/format/csv/delimiter/,/compression/gzip/adultcontent/1/"
 		if r.URL.String() != expectedUrl {
